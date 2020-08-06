9ee3784cc5d92b3fe0a88cbef3462a6ef2152aaa
Darbaz Ali <darbaz.a.h@gmail.com>
2020-08-06 09:56:38 +0300

    fetch data from API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { render } from "react-dom";
 
-import Header from './components/Header';
+
+import Header from "./components/Header";
 const App = () => {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const api_url = "https://www.breakingbadapi.com/api/characters";
+
+  useEffect(() => {
+    fetch(api_url)
+      .then((res) => res.json())
+      .then((data) => {
+        setItems(data);
+        setIsLoading(false);
+      });
+  });
+
   return (
     <div>
       <Header />
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
-    return (
-        <header>
-            <h1>Breaking Bad</h1>
-        </header>
-    )
-}
+  return (
+    <header>
+      <h1>Breaking Bad</h1>
+    </header>
+  );
+};
 
-export default Header
-
+export default Header;
