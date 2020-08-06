08aa148eded1b61f29b01a1acebff168e5ed14bb
Darbaz Ali <darbaz.a.h@gmail.com>
2020-08-06 10:04:15 +0300

    add CharacterGrid

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { render } from "react-dom";
 
 
 import Header from "./components/Header";
+import CharacterGrid from './components/CharacterGrid';
 const App = () => {
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
@@ -20,6 +21,10 @@ const App = () => {
   return (
     <div>
       <Header />
+      <CharacterGrid 
+        isLoading={isLoading}
+        items={items}
+      />
     </div>
   );
 };
